3404fabcde3008cd700586ba7664eb09474b3a1b
Antonio Jr <acdcjunior@gmail.com>
2017-06-19 23:28:56 -0300

    refactor(home): mover template para decorator

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,7 +3,26 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  template: `
+    <ion-header>
+      <ion-navbar>
+        <button ion-button menuToggle>
+          <ion-icon name="menu"></ion-icon>
+        </button>
+        <ion-title>Home</ion-title>
+      </ion-navbar>
+    </ion-header>
+
+    <ion-content padding>
+      <h3>Ionic Menu Starter</h3>
+
+      <p>
+        If you get lost, the <a href="http://ionicframework.com/docs/v2">docs</a> will show you the way.
+      </p>
+
+      <button ion-button secondary menuToggle>Toggle Menu</button>
+    </ion-content>
+  `
 })
 export class HomePage {
 
