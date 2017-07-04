4e191f3ec012fb8d61bb09ba69b1880dc7762a50
Antonio Jr <acdcjunior@gmail.com>
2017-07-03 23:52:54 -0300

    feat(home): incluir botoes de vacinas e de login

--- src/pages/home-botoes/home-botoes.ts
+++ src/pages/home-botoes/home-botoes.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ListPage } from "../list/list";
 
 @Component({
   selector: 'page-home-botoes',
@@ -9,7 +10,12 @@ import { NavController, NavParams } from 'ionic-angular';
     </ion-header>
 
     <ion-content padding>
-      <h1>HELLO BOTOES</h1>
+      <h1 class="pergunta">Que vacinas deveria ter tomado...</h1>
+      <button ion-button large block class="botao-pessoa" (click)="abrirFilha()">Minha filha</button>
+      <button ion-button large block class="botao-pessoa" (click)="abrirFilho()">Meu filho</button>
+      <button ion-button large block class="botao-pessoa" (click)="abrirEu()">Eu</button>
+      
+      <vacinas-login></vacinas-login>
     </ion-content>
   `
 })
@@ -18,8 +24,17 @@ export class HomeBotoesPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HomeBotoesPage');
+  abrirFilha() {
+    // console.log('filha!');
+    this.navCtrl.push(ListPage, {item: 'Filha'});
+  }
+  abrirFilho() {
+    // console.log('filho!');
+    this.navCtrl.push(ListPage, {item: 'Filho'});
+  }
+  abrirEu() {
+    // console.log('eu!');
+    this.navCtrl.push(ListPage, {item: 'Eu'});
   }
 
 }
