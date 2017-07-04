f81010a249eebf2c056f97594652d1683c64feba
Antonio Jr <acdcjunior@gmail.com>
2017-07-03 23:50:18 -0300

    feat(list): traduzir termos

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -22,7 +22,7 @@ export class ListPage {
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Item ' + i,
-        note: 'This is item #' + i,
+        note: 'Este é o item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
