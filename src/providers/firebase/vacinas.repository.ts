1e57cf1f3197b200bd654c4a21c8de6e9b376fc4
Antonio Jr <acdcjunior@gmail.com>
2017-07-05 12:05:49 -0300

    feat: criar vacina repo e classes de dominio

--- src/providers/firebase/vacinas.repository.ts
+++ src/providers/firebase/vacinas.repository.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import {Observable} from "rxjs/Observable";
+import 'rxjs/add/operator/concatMap';
+import "rxjs/add/operator/filter";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/first';
 
 export class Opcao {
-  nomeOpcao: string;
-  tipo: string;
-  subOpcoes: string[];
+  chave: string;
+  descricao: string;
+  subOpcoes: SubOpcao[];
+}
+export class SubOpcao {
+  titulo: string;
+  chave: string;
 }
 export class IdadeDose {
   idadeDose: string[];
@@ -23,6 +31,11 @@ export class VacinasRepository {
   getOpcoes(): Observable<Opcao[]> {
     return this.afd.list('/opcoes/');
   }
+
+  getOpcao(chave: string): Observable<Opcao> {
+    return this.getOpcoes().concatMap((x: Opcao[]) => x).filter((x: Opcao) => x.chave === chave).first();
+  }
+
   getDoses(): Observable<IdadeDose[]> {
     return this.afd.list('/doses/');
   }
