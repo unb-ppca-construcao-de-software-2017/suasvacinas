4a975f026292d10f79135778f6c7908079ca7fb2
Antonio Jr <acdcjunior@gmail.com>
2017-07-09 15:37:55 -0300

    feat(caderneta): incluir link para caderneta nova em caderneta-miolo

--- src/ts/caderneta/caderneta-miolo.component.ts
+++ src/ts/caderneta/caderneta-miolo.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {AutenticacaoService} from "../firebase/autenticacao.service";
+import {NavController} from "ionic-angular";
+import {CadernetaNovaComponent} from "./caderneta-nova.component";
 
 @Component({
   selector: 'vacinas-caderneta-miolo',
@@ -9,9 +12,18 @@ import { Component } from '@angular/core';
       <ion-row>
         <ion-col offset-2 col-8 class="texto-centralizado">
         
-          <h1>Caderneta</h1>
+          <h1>Cadernetas</h1>
+
+          <br>
           
-          Fazendo agora...
+          <button ion-button outline item-end icon-left (click)="novaCaderneta()">
+            <ion-icon name="add"></ion-icon>
+            Nova Caderneta
+          </button>
+          
+          <br><br>
+          
+          Você não tem nenhuma caderneta criada.
 
         </ion-col>
       </ion-row>
@@ -19,5 +31,13 @@ import { Component } from '@angular/core';
   `
 })
 export class CadernetaMioloComponent {
+
+  constructor(private autenticacaoService: AutenticacaoService, private navCtrl: NavController) {
+    autenticacaoService.isAutenticado();
+  }
+
+  novaCaderneta(): void {
+    this.navCtrl.push(CadernetaNovaComponent);
+  }
 
 }
