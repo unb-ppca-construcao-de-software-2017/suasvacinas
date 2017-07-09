419fd574cdea0af6242a876ea0626f3629b6e2b8
Antonio Jr <acdcjunior@gmail.com>
2017-07-09 15:31:20 -0300

    feat(caderneta): incluir interface firebase

--- src/ts/caderneta/caderneta.model.ts
+++ src/ts/caderneta/caderneta.model.ts
@@ -1,6 +1,15 @@
-export class Caderneta {
+export interface ICaderneta {
+  $key?: string;
 
+  nome: string;
+  datanascimento: string;
+  sexo: string;
   dosesTomadas: any;
+}
+
+export class Caderneta implements ICaderneta {
+
+  dosesTomadas: any = {};
 
   constructor(public nome: string, public datanascimento: string, public sexo: string) {
   }
