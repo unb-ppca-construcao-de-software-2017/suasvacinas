9c31eff60fde8e44c70f6f5d3370e621e6078177
Antonio acdc Jr <acdcjunior@gmail.com>
2017-07-10 17:23:26 -0300

    feat(caderneta): adicionar placeholders e ajustar padding

--- src/ts/caderneta/caderneta-nova.component.ts
+++ src/ts/caderneta/caderneta-nova.component.ts
@@ -27,19 +27,19 @@ import {HomeComponent} from "../home/home.component";
     <ion-content padding>
       <ion-grid>
         <ion-row>
-          <ion-col offset-1 col-10>
+          <ion-col col-12>
             <h1 class="centro">Nova caderneta</h1>
 
             <form #cadastroForm="ngForm">
               <ion-list>
                 <ion-item>
                   <ion-label stacked>Nome da caderneta/pessoa</ion-label>
-                  <ion-input type="text" required class="form-control" name="nome" minlength="3" [(ngModel)]="nome" #nomeDom="ngModel"></ion-input>
+                  <ion-input type="text" required class="form-control" name="nome" minlength="3" [(ngModel)]="nome" #nomeDom="ngModel" placeholder="Digite aqui o nome da pessoa/caderneta"></ion-input>
                 </ion-item>
 
                 <ion-item>
                   <ion-label stacked>Data de nascimento (opcional)</ion-label>
-                  <ion-datetime displayFormat="DD MMMM YYYY" [(ngModel)]="data_nascimento" name="data-nascimento" cancelText="Cancelar" doneText="Selecionar"></ion-datetime>
+                  <ion-datetime displayFormat="DD MMMM YYYY" [(ngModel)]="data_nascimento" name="data-nascimento" cancelText="Cancelar" doneText="Selecionar" placeholder="Clique aqui para selecionar a data"></ion-datetime>
                 </ion-item>
               
                 <ion-item>
